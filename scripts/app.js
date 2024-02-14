331f490169471a75f6f7dda995a35e7c89d78498
Jeriah Marson <jeriahmarson@gmail.com>
2024-02-14 14:11:43 -0800

    publish

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+let eyeD = document.getElementById("eyeD");
+let fName = document.getElementById("fName");
+let lName = document.getElementById("lName");
+let email = document.getElementById("email");
+let age = document.getElementById("age");
+let height = document.getElementById("height");
+
+import data from '../data/data.json';
+
