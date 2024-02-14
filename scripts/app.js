365974228ae39fe7e9bf6e2845dc9e2ff2b73a4c
Jeriah Marson <jeriahmarson@gmail.com>
2024-02-14 15:39:24 -0800

    bleh

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,24 @@ let lName = document.getElementById("lName");
 let email = document.getElementById("email");
 let age = document.getElementById("age");
 let height = document.getElementById("height");
+let eyeDBtn = document.getElementById("eyeDBtn");
+let fNameBtn = document.getElementById("fNameBtn");
+let lNameBtn = document.getElementById("lNameBtn");
+let ageBtn = document.getElementById("ageBtn");
+let heightBtn = document.getElementById("heightBtn");
+let tenBtn = document.getElementById("tenBtn");
+let twentyBtn = document.getElementById("twentyBtn");
+let thirtyBtn = document.getElementById("thirtyBtn");
+let fourtyBtn = document.getElementById("fourtyBtn");
+let fiftyBtn = document.getElementById("fiftyBtn");
 
-import data from '../data/data.json';
+// import data from '../data/data.json';
 
+const getData = async () => {
+    const promise = await fetch('../data/data.json');
+    const data = await promise.text();
+    console.log(data);
+    return data.People
+}
+
+getData();
